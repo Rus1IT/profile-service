d831ca39c179e1285749fcd4b71308b33c814307
Rus1IT <ruslankoldasov1@gmail.com>
2025-11-01 16:57:59 +0500

    Adding new Entity Account

---
+++ src/main/java/com/cashpilot/userservice/entity/Account.java
@@ -0,0 +1,57 @@
+package com.cashpilot.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "accounts", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_profile_id", "bank_name"})
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+
+    @Id
+    @UuidGenerator
+    @Column(name = "account_id")
+    private UUID accountId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_profile_id", nullable = false)
+    private UserProfile userProfile;
+
+    @Column(name = "bank_name", nullable = false, length = 100)
+    private String bankName;
+
+    @Column(name = "account_name", nullable = false, length = 100)
+    private String accountName;
+
+    @Column(name = "currency", nullable = false, length = 3)
+    private String currency;
+
+    // Можно добавить поля для отслеживания дат
+    @Column(name = "first_transaction_date")
+    private Instant firstTransactionDate; // Самая ранняя транзакция
+
+    @Column(name = "last_transaction_date")
+    private Instant lastTransactionDate; // Самая последняя транзакция
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+}
